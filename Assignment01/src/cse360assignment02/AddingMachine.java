d848c7726c9a3cd89e8ab036dc40e94f2a15943b
Charis Han <chan25@asu.edu>
2020-10-04 22:16:43 -0700

    Second version

--- Assignment01/src/cse360assignment02/AddingMachine.java
+++ Assignment01/src/cse360assignment02/AddingMachine.java
@@ -3,25 +3,36 @@ package cse360assignment02;
 public class AddingMachine {
 	
 	private int total;
+	private String history = "0"; //history should start from the initial 0
 	  
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
 	  }
 	  
+	  //return the current total
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
+	  //add the parameter to the total variable
 	  public void add (int value) {
+		  total = total + value;
+		  history += " + " + value;
 	  }
-
+	  
+	  //subtract the parameter from the total variable
 	  public void subtract (int value) {
+		  total = total - value;
+		  history += " - " + value;
 	  }
-
+	  
+	  //history of the transactions 
 	  public String toString () {
-	    return "";
+	    return history;
 	  }
-
+	  
+	  //clear "memory"
 	  public void clear() {
+		  history = "0";
 	  }
 }
